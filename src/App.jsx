91c6bce1935436ba4b47f368a5173137e562a963
Hammad Shoaib <hammadsami119@gmail.com>
2024-03-11 15:27:55 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Home from "./pages/Homepage";
-// import Explore from "./pages/Explore";
+// import Home from "./pages/Homepage";
+import Explore from "./pages/Explore";
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Home />
+        <Explore />
+        {/* <Home /> */}
       </div>
     </>
   );
