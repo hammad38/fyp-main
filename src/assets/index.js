8030a91b7298d7c9477bf03cdf6125d0d9d2834b
Hammad Shoaib <96943053+hammad38@users.noreply.github.com>
2024-04-30 19:15:20 +0000

    .

--- src/assets/index.js
+++ src/assets/index.js
@@ -41,8 +41,8 @@ import raindrop from "./collaboration/raindrop.png";
 import slack from "./collaboration/slack.png";
 
 import service1 from "./services/service-1.png";
-import service2 from "./services/service-2.png";
-import service3 from "./services/service-3.png";
+import service2 from "./services/service-2.jpg";
+import service3 from "./services/service-3.jpg";
 import service4 from "./services/service-4.png";
 import service5 from "./services/service-5.png";
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -66,10 +66,11 @@ const Services = () => {
               </div>
 
               <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
-                <h4 className="h4 mb-4">Photo editing</h4>
+                <h4 className="h4 mb-4">Ready to level up your UI game?</h4>
                 <p className="body-2 mb-[3rem] text-n-3">
-                  Automatically enhance your photos using our AI app&apos;s
-                  photo editing feature. Try it now!
+                A nifty collection of pre-built Flutter widgets/screens to speed up your UI development!
+
+
                 </p>
               </div>
 
@@ -78,10 +79,9 @@ const Services = () => {
 
             <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
               <div className="py-12 px-4 xl:px-8">
-                <h4 className="h4 mb-4">Video generation</h4>
+                <h4 className="h4 mb-4">Stunning|Flutter components</h4>
                 <p className="body-2 mb-[2rem] text-n-3">
-                  The world’s most powerful AI photo and video art generation
-                  engine. What will you create?
+                Hand crafted widgets for developers that want to speed up their workflow
                 </p>
 
                 <ul className="flex items-center justify-between">
